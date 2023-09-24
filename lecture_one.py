903673f5b84b5d2b2b3ebfd8711be46433bb5731
Faizan Alam <51821426+Faizan-Alam-1@users.noreply.github.com>
2023-09-24 21:43:39 +0530

    Add files via upload

---
+++ lecture_one.py
@@ -0,0 +1,49 @@
+
+#  This very imp code  
+
+# print("hey , ram" + " " + "i'm , faizan" )   # this is string concatnation 
+
+
+# print("what your name?") 
+# input() 
+
+
+
+   
+# print(len("Ram")) # will give size if str.  --- 3
+
+
+# print("What your name ?")
+ # whatever user give in outpu 
+
+# print(len( input()))
+
+
+
+# variable 
+
+# print("what your name?") 
+# a = input() # faizan
+# a = "ram"
+# print(a)
+
+
+# assignment is 
+
+# 1. swap  
+# you take input from user1 --- a  lets   ram 
+# you take input from user2 --- b   lets  faizan
+# ouput will faizan and ram   
+
+# loop in python 
+# if  in python  else 
+
+
+
+
+
+
+
+
+
+
