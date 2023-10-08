8ca6151254982da2da2618b6069c14b14025dab9
Faizan Alam <51821426+Faizan-Alam-1@users.noreply.github.com>
2023-10-08 21:39:43 +0530

    Add files via upload

---
+++ Lect_3/assginment_coding.py
@@ -0,0 +1,4 @@
+# write program to calculate Body Mass Index (BMI)
+# take input mass or weight 
+# take input height 
+# formula for BMI = (mass or weight)/(height*height)
---
+++ Lect_3/lect_3.py
@@ -0,0 +1,77 @@
+# + , - * , / , **  
+
+# PEMDAS --
+
+# ()    
+# ** , *  , # /
+# +
+# -  
+
+
+# num1 = 2
+# num2 = 2
+
+# sum = num1+num2
+# print(sum)
+
+# sub = num1-num2 
+# print(sub)
+
+
+# multi = num1*num2
+# print(multi)
+
+# div =  num1/num2
+# div =  num1//num2
+# print(div)
+
+# expo = num1**num2
+# print(expo)
+
+
+
+# num = (3* 3 + 3 / 3 - 3)
+
+# print(num)
+
+
+
+# compartion opretors
+
+# > 
+# < 
+
+# >=  10>=          
+
+# <=
+
+# ==   # = 
+
+
+# num = 3  
+
+# print(num == 3)  # is it equal ?
+
+# num1 = 10
+# num2 = 90
+
+# print(num2 >= num1)  # if num1 and num2 is equal or  num2 > num1
+
+
+# num1 = 100
+# num2 = 90
+
+# print(num1<=num2)
+
+
+# condition 
+
+name  = "what your result\n"
+result = int(input(name))
+
+if (result >= 80):
+    print("you will get bike")
+else :
+    print("you will not get bike")    
+
+
