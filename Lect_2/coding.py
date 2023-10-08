7b73c36ffc8b86233518f9449c58d544407340d6
Faizan Alam <51821426+Faizan-Alam-1@users.noreply.github.com>
2023-10-08 21:41:42 +0530

    assgimnet_coding.py

--- Lect_2/coding.py
+++ Lect_2/coding.py
@@ -11,5 +11,27 @@ ouput -- 7
 
 
 
+user = "Enter the number\n"
+ans  = input(user)
+char1 =  ans[0] # 1
+char2 = ans[1] # 2
+char3 = ans[2] # 3
+char4 = ans[3]  # 4
+char5 = ans[4] # 5
+
+# typecaseting   
+char1_int = int(char1)
+char2_int = int(char2)
+char3_int = int(char3)
+char4_int = int(char4)
+char5_int = int(char5)
+
+sum = char1_int+char2_int+char3_int+char4_int+char5_int 
+
+print(sum)
+
+
+
+
 
 
