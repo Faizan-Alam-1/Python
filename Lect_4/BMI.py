2bea051fdd6e227b6693b031b00d557a231b6bb3
Faizan Alam <51821426+Faizan-Alam-1@users.noreply.github.com>
2023-10-22 19:16:29 +0530

    Add files via upload

---
+++ Lect_4/BMI.py
@@ -0,0 +1,15 @@
+# write program to calculate Body Mass Index (BMI)
+# take input mass or weight 
+# take input height 
+# formula for BMI = (mass or weight)/(height*height)
+
+# 2 = 4  --- 2*2 , 2 power 2 , 2**2 = 4
+
+
+mass =  input("Give your weigth\n")
+height = input("Give your height\n")
+
+BMI = int((mass))/ int((height))**int((height))  
+
+print(BMI)
+ 
---
+++ Lect_4/recap.py
@@ -0,0 +1,35 @@
+# PEMDAS
+# compation opretors 
+
+# =  OR ==
+
+
+# condition 
+
+#  WE CAN COMPAIR SAME DATA TYPE.
+
+num1 = input("enter the any number b/w 1 to  5\n")
+str_to_int_num1 = int(num1)
+
+if   str_to_int_num1 == 10:
+    print("10")
+
+if str_to_int_num1  == 5:
+    print("5")
+
+if str_to_int_num1  == 1:
+    print("1")
+
+if str_to_int_num1  == 2 :
+    print("2") 
+
+if str_to_int_num1 == 3:
+    print("3")
+
+if str_to_int_num1 == 4:
+    print("4")
+       
+
+    
+
+
