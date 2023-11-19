e0fbcbe4267e0bdbb11f66885b7b8433055a603d
Faizan Alam <faianalam2030@gmail.com>
2023-11-19 22:25:26 +0530

    Lect_7

---
+++ Lect_7/Assignment.py
@@ -0,0 +1,6 @@
+# Print even number from 1 to 100, using loop and store ur even number in list and then print the even number.print.print.
+
+
+num = int(input("Enter number"))
+
+# write code 
--- Lect_7/main.py
+++ Lect_7/main.py
@@ -1,1 +1,57 @@
-print("Hello ")
+
+# My_list = [ 2 , "rrrr" , 'd' , 6.8]
+        #  0     1        2     3
+
+
+
+
+
+
+
+
+# nums = [ 2, 5,6,8,9]
+
+# # loop 
+
+# sum  = 0
+# for item in nums :
+#     sum += item 
+
+# print(sum)    
+
+
+
+# tuple  
+
+
+# my_tuple = ("ram" , 33, 7.8)
+# my_tuple[0] = "Faizan"
+# print(my_tuple)
+
+    
+
+# indexing 
+
+
+# myList = [3,5,6,7,8,9,10,12,36,67] 
+
+
+
+# print(myList[2::3]) # 6,9,36
+
+
+
+
+
+# range(1 , 100)
+
+
+# for i in  range(1,101):
+#     print(i)
+
+# appen()
+
+
+mylist = [2,4,5,7]
+mylist.append(100)
+print(mylist)
